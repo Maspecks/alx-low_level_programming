0a8f30d194be1dfacc13289feebe0b4e6c3466c4
Maspecks <mwaniki_g@yahoo.com>
2022-12-13 22:26:18 -0800

    Task0

---
+++ 0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints characters
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	{
+		printf("_");
+		printf("p");
+		printf("u");
+		printf("t");
+		printf("c");
+		printf("h");
+		printf("a");
+		printf("r");
+	}
+	printf("\n");
+	return (0);
+}

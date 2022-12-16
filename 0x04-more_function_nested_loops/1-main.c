ceea0e1d8f075612123e9d28142c0628a50b5757
Maspecks <mwaniki_g@yahoo.com>
2022-12-15 18:58:33 -0800

    Update

--- 0x04-more_function_nested_loops/1-main.c
+++ 0x04-more_function_nested_loops/1-main.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 
 /**
- * main - checks the code
+ * main - check the code
  *
  * Return: Always 0
  */
-
 int main(void)
 {
 	char c;
--- 0x04-more_function_nested_loops/6-main.c
+++ 0x04-more_function_nested_loops/6-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - checks the code
  *
  * Return: Always 0
  */
@@ -9,7 +9,7 @@ int main(void)
 {
 	print_line(0);
 	print_line(2);
-	print_line(10);
+	print-line(10);
 	print_line(-4);
 	return (0);
 }

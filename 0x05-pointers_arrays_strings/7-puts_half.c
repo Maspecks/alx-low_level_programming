6a63441599fb13ea1201b02850a86495a5115e2f
Maspecks <mwaniki_g@yahoo.com>
2022-12-19 15:28:23 -0800

    Task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints half a string
+ * @str: character to be printed
+ */
+void puts_half(char *str)
+{
+	int i, len = strlen(str);
+
+	for (i = len / 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}

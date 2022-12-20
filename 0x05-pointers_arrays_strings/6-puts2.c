27a258f163e7c81559b717043763dc0be34ff74d
Maspecks <mwaniki_g@yahoo.com>
2022-12-20 01:12:47 -0800

    Task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,14 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
 	}
 	_putchar('\n');
 }

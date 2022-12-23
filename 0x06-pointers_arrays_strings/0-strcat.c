e86c23233a85b88f4837c4794d0306b22e0aac74
Maspecks <mwaniki_g@yahoo.com>
2022-12-22 17:41:58 -0800

    Task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,17 +10,9 @@ char *_strcat(char *dest, char *src);
 {
 	int i = 0, j = 0;
 
-	while (*(dest + i) != '\0')
-	{
-		i++
-	}
-	while (j >= 0)
-	{
-		*(dest + i) = *(src + j);
-		if (*(src + j) == '\0')
-			break;
+	while (dest[j++])
 		i++;
-		j++;
-	}
+	for (j = 0; src[j]; j++)
+		dest[i++] = src[j];
 	return (dest);
 }

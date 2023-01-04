bc0a05ad2041b71d61fb2fe86af13b7f160da6b5
Maspecks <mwaniki_g@yahoo.com>
2023-01-03 22:34:07 -0800

    Task4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,17 +8,13 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-int i;
-int j;
-for (i = 0; s[i] != '\0'; i++)
-{
-for (j = 0; accept[j] != '\0'; j++)
-{
-if (s[i] == accept[j])
-{
-return (s + i);
-}
-}
-}
-return (0);
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; j++)
+	{
+		if (s[i] == accept[j])
+		{
+			return (0);
+		}
+	}
 }

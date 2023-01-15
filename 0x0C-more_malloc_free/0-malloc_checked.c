54bf3241caf6051a30cdf4466314141e9a82262a
Maspecks <mwaniki_g@yahoo.com>
2023-01-14 17:35:15 -0800

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - a function to allocate memory
+ * @b: unsigned inteeger
+ * Return: a pointerg void
+ */
+void *malloc_checked(unsigned int b)
+{
+	vod *g;
+
+	g = malloc(b);
+
+	if (g == NULL)
+		exit(98);
+
+	return (p);
+}
